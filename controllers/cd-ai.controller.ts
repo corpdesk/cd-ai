bc7c2439d909a4b92c99dcd280859ce6121caa6c
“georemo” <george.oremo@gmail.com>
2025-09-01 15:31:28 +0300

    services and modules created

---
+++ controllers/cd-ai.controller.ts
@@ -0,0 +1,45 @@
+import { BaseService } from '../../../sys/base/base.service';
+import { CdAiService } from '../services/cd-ai.service';
+import { CdAiUsageLogsService } from '../services/cd-ai-usage-logs.service';
+
+export class CdAiController {
+  constructor() {
+    // TODO: initialize controller
+  }
+
+  async Create(): Promise<void> {
+    // TODO: implement
+  }
+
+  async Get(): Promise<void> {
+    // TODO: implement
+  }
+
+  async GetType(): Promise<void> {
+    // TODO: implement
+  }
+
+  async GetCount(): Promise<void> {
+    // TODO: implement
+  }
+
+  async Update(): Promise<void> {
+    // TODO: implement
+  }
+
+  async Delete(): Promise<void> {
+    // TODO: implement
+  }
+
+  async PromptQuery(): Promise<void> {
+    // TODO: implement
+  }
+
+  async CheckTokenBalance(): Promise<void> {
+    // TODO: implement
+  }
+
+  async GetUserProfile(): Promise<void> {
+    // TODO: implement
+  }
+}
