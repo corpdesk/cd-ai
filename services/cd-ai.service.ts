c651a21caf2b6a3af7ff89292bec6035db9a3365
“georemo” <george.oremo@gmail.com>
2025-09-04 18:09:08 +0300

    services and modules created

---
+++ services/cd-ai.service.ts
@@ -0,0 +1,132 @@
+import { BaseService } from '../../../sys/base/base.service';
+import { CdAiModel } from '../models/cd-ai.model.ts.model';
+import { CdAiTypeModel } from '../models/cd-ai-type.model.ts.model';
+import { CdAiUsageLogsModel } from '../models/cd-ai-usage-logs.model.ts.model';
+import { CdAiUsageLogsTypeModel } from '../models/cd-ai-usage-logs-type.model.ts.model';
+
+export class CdAiService {
+  logger: Logging;
+  b: BaseService;
+  cdToken: string;
+  uid: number;
+  serviceModel: CdAiModel;
+  svSess: SessionService;
+  validationCreateParams: any;
+  cRules: object;
+
+  constructor() {
+    // TODO: initialize service
+  }
+
+  constructor() {
+    this.b = new BaseService();
+    // this.logger = new Logging();
+    this.serviceModel = new AbcdModel();
+  }
+
+  async create(data: any): Promise<any> {
+    // TODO: implement create logic
+    return data;
+  }
+
+  // <<cd:method:validateCreate:start>>
+  async validateCreate(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:validateCreate:end>>
+
+  // <<cd:method:cdAiExists:start>>
+  async cdAiExists(): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:cdAiExists:end>>
+
+  // <<cd:method:getCdAiCount:start>>
+  async getCdAiCount(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:getCdAiCount:end>>
+
+  // <<cd:method:getCdAiQB:start>>
+  async getCdAiQB(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:getCdAiQB:end>>
+
+  // <<cd:method:getCdAiType:start>>
+  async getCdAiType(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:getCdAiType:end>>
+
+  // <<cd:method:getCdAiProfile:start>>
+  async getCdAiProfile(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:getCdAiProfile:end>>
+
+  // <<cd:method:getCdAiProfileByToken:start>>
+  async getCdAiProfileByToken(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:getCdAiProfileByToken:end>>
+
+  // <<cd:method:getScopedCdAi:start>>
+  async getScopedCdAi(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:getScopedCdAi:end>>
+
+  // <<cd:method:updateCdAiProfile:start>>
+  async updateCdAiProfile(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:updateCdAiProfile:end>>
+
+  async update(id: string, data: any): Promise<any> {
+    // TODO: implement update logic
+    return data;
+  }
+
+  delete(req, res) {
+    const q = this.b.getQuery(req);
+    const serviceInput = {
+      serviceModel: AbcdModel,
+      docName: "AbcdService::delete",
+      cmd: {
+        action: "delete",
+        query: q,
+      },
+      dSource: 1,
+    };
+
+    this.b.delete$(req, res, serviceInput).subscribe((ret) => {
+      this.b.cdResp.data = ret;
+      this.b.respond(req, res);
+    });
+  }
+
+  // <<cd:method:activateCdAi:start>>
+  async activateCdAi(): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:activateCdAi:end>>
+
+  // <<cd:method:promptQuery:start>>
+  async promptQuery(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:promptQuery:end>>
+
+  // <<cd:method:checkTokenBalance:start>>
+  async checkTokenBalance(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:checkTokenBalance:end>>
+
+  // <<cd:method:getUserProfile:start>>
+  async getUserProfile(req: Request, res: Response): Promise<void> {
+    // TODO: implement
+  }
+  // <<cd:method:getUserProfile:end>>
+}
