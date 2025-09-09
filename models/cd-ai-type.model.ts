eade4f0ce5769a5e25bd181170366c190dd8a474
“georemo” <george.oremo@gmail.com>
2025-09-09 21:55:41 +0300

    services and modules created

---
+++ models/cd-ai-type.model.ts
@@ -0,0 +1,43 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+@Entity({
+  name: "cd_ai_type",
+  synchronize: false,
+})
+export class CdAiTypeModel {
+  @PrimaryGeneratedColumn({
+    name: "cd_ai_type_type_id",
+  })
+  cdAiTypeTypeId!: number;
+
+  @Column({
+    name: "cd_ai_type_type_guid",
+    default: "uuid",
+  })
+  cdAiTypeTypeGuid!: string;
+
+  @Column({
+    name: "cd_ai_type_type_name",
+    nullable: true,
+  })
+  cdAiTypeTypeName?: string;
+
+  @Column({
+    name: "cd_ai_type_description_type",
+  })
+  cdAiTypeDescriptionType!: string;
+
+  @Column({
+    name: "cd_ai_type_doc_type_id",
+    nullable: true,
+  })
+  cdAiTypeDocTypeId?: number;
+
+  @Column({
+    name: "cd_ai_type_enabled_type",
+    nullable: true,
+    default: true,
+  })
+  cdAiTypeEnabledType?: boolean;
+}
